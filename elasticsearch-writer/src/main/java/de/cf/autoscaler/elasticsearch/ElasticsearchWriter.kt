6623b9ee60385ea5055544e6feb7fa542ac59957
René Schollmeyer <rschollmeyer@evoila.de>
2018-03-08 15:47:15 +0100

    feat: add elasticsearch-writer to send logMessages from kafka to elasticsearch

---
+++ elasticsearch-writer/src/main/java/de/cf/autoscaler/elasticsearch/ElasticsearchWriter.kt
@@ -0,0 +1,64 @@
+package de.cf.autoscaler.elasticsearch
+
+import de.cf.autoscaler.elasticsearch.beans.ElasticsearchPropertiesBean
+import de.cf.autoscaler.elasticsearch.kafka.LogMessageConsumer
+import de.cf.autoscaler.kafka.KafkaPropertiesBean
+import de.cf.autoscaler.kafka.messages.LogMessage
+import org.elasticsearch.client.transport.TransportClient
+import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.common.transport.TransportAddress
+import org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder
+import org.elasticsearch.transport.client.PreBuiltTransportClient
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import java.net.InetAddress
+import java.sql.Date
+import javax.annotation.PostConstruct
+
+
+/**
+ * Created by reneschollmeyer, evoila on 07.03.18.
+ */
+@Component
+class ElasticsearchWriter @Autowired constructor(
+        private val kafkaPropertiesBean: KafkaPropertiesBean,
+        private val elasticsearchPropertiesBean: ElasticsearchPropertiesBean
+){
+
+    private lateinit var connection: TransportClient
+
+    @PostConstruct
+    fun executeElasticSearchWriter() {
+        logMessageConsumerRunner()
+    }
+
+    private fun logMessageConsumerRunner() {
+        val logMessageConsumer = LogMessageConsumer("writer_log_messages",
+                kafkaPropertiesBean, this)
+        logMessageConsumer.startConsumer()
+    }
+
+    private fun connect(): TransportClient {
+        var settings: Settings = Settings.builder()
+                .put("cluster.name", elasticsearchPropertiesBean.clusterName)
+                .build()
+
+        connection = PreBuiltTransportClient(settings).addTransportAddress(
+                TransportAddress(InetAddress.getByName(elasticsearchPropertiesBean.host),
+                        elasticsearchPropertiesBean.port))
+
+        return connection
+    }
+
+    fun writeLogMessage(data: LogMessage) {
+        val response = connect()
+                .prepareIndex(Date(data.timestamp).toString(),
+                        data.logMessageType,
+                        data.appId)
+                .setSource(jsonBuilder()
+                        .startObject()
+                        .field("message", data.logMessage)
+                        .endObject())
+                .get()
+    }
+}
---
+++ elasticsearch-writer/src/main/java/de/cf/autoscaler/elasticsearch/constants/LogMessageFields.kt
@@ -0,0 +1,12 @@
+package de.cf.autoscaler.elasticsearch.constants
+
+/**
+ * Created by reneschollmeyer, evoila on 07.03.18.
+ */
+
+enum class LogMessageFields(val logMessage: String) {
+
+    LOGMESSAGE("logMessage"),
+    LOGMESSAGETYPE("logMessageType"),
+    APPID("appId")
+}
