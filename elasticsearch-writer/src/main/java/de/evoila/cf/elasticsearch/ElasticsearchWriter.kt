8552ccd17cf42da78c73fced7fcf1da9e7cf7b88
René Schollmeyer <rschollmeyer@evoila.de>
2018-12-21 12:52:03 +0100

    feat: add fail safety to the elasticsearch writer in case elasticsearch crashes

--- elasticsearch-writer/src/main/java/de/evoila/cf/elasticsearch/ElasticsearchWriter.kt
+++ elasticsearch-writer/src/main/java/de/evoila/cf/elasticsearch/ElasticsearchWriter.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.uuid.Generators
 import de.evoila.cf.autoscaler.kafka.KafkaPropertiesBean
 import de.evoila.cf.autoscaler.kafka.messages.LogMessage
+import de.evoila.cf.elasticsearch.security.ElasticsearchPortAvailabilityVerifier
+import de.evoila.cf.elasticsearch.writer.beans.ElasticsearchPropertiesBean
 import de.evoila.cf.elasticsearch.writer.beans.GrokPatternBean
 import de.evoila.cf.elasticsearch.writer.connection.ElasticsearchRestClientFactory
 import de.evoila.cf.elasticsearch.writer.kafka.LogMessageConsumer
@@ -12,10 +14,12 @@ import de.evoila.cf.elasticsearch.writer.model.GrokPatternMatcher
 import groovy.json.JsonBuilder
 import org.apache.http.entity.ContentType
 import org.apache.http.nio.entity.NStringEntity
+import org.elasticsearch.client.Request
+import org.elasticsearch.client.RestClient
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.sql.Date
-import java.util.*
 import javax.annotation.PostConstruct
 
 
@@ -25,19 +29,30 @@ import javax.annotation.PostConstruct
 @Component
 class ElasticsearchWriter @Autowired constructor(
         private val kafkaPropertiesBean: KafkaPropertiesBean,
-        grokPatternBean: GrokPatternBean){
+        grokPatternBean: GrokPatternBean,
+        private val elasticsearchProperties: ElasticsearchPropertiesBean){
+
+    private val CONNECTION_TIMEOUT = 10
+
+    private val log = LoggerFactory.getLogger(ElasticsearchWriter::class.java!!)
 
     private val mapper = ObjectMapper()
     private val matcher = GrokPatternMatcher(grokPatternBean)
     private var writerObjectMap = hashMapOf<String, ElasticsearchWriterObject>()
+
+    private lateinit var client: RestClient
 
     private lateinit var writerObject: ElasticsearchWriterObject
 
     @Autowired
     private lateinit var elasticsearchRestClientFactory: ElasticsearchRestClientFactory
 
+    @Autowired
+    private lateinit var availabilityVerifier: ElasticsearchPortAvailabilityVerifier
+
     @PostConstruct
     fun executeElasticSearchWriter() {
+        client = elasticsearchRestClientFactory.getRestClientConnection()
         logMessageConsumerRunner()
     }
 
@@ -52,14 +67,35 @@ class ElasticsearchWriter @Autowired constructor(
             var jsonString = mapper.writeValueAsString(writerObjectMap[data.appId].let { it })
             writerObjectMap.remove(data.appId)
 
-            if(jsonString != "null") {
-                val entity = NStringEntity(jsonString, ContentType.APPLICATION_JSON)
+            try {
+                if(jsonString != "null") {
+                    val entity = NStringEntity(jsonString, ContentType.APPLICATION_JSON)
 
-                var client = elasticsearchRestClientFactory.getRestClientConnection()
+                    var endpoint = "/" + Date(data.timestamp).toString() + "/logMessages/" + Generators.randomBasedGenerator().generate()
 
-                var endpoint = "/" + Date(data.timestamp).toString() + "/logMessages/" + Generators.randomBasedGenerator().generate()
+                    var logMessageRequest = Request("PUT", endpoint)
+                    logMessageRequest.apply {
+                        this.entity = entity
+                    }
 
-                client.performRequest("PUT", endpoint, Collections.emptyMap(), entity)
+                    client.performRequest(logMessageRequest)
+                }
+            } catch(ex: Exception) {
+                log.error(ex.message)
+
+                for (i in 0 until CONNECTION_TIMEOUT) {
+                    log.info("Trying to reconnect Elasticsearch, attempt: " + (i+1))
+
+                    var available = availabilityVerifier
+                            .verifyServiceAvailability(elasticsearchProperties.host, elasticsearchProperties.port, true)
+
+                    if(available) {
+                        client = elasticsearchRestClientFactory.getRestClientConnection()
+                        log.info("Connection to Elasticsearch established.")
+
+                        break
+                    }
+                }
             }
 
             val matchMap = if(data.sourceType == "RTR") {
---
+++ elasticsearch-writer/src/main/java/de/evoila/cf/elasticsearch/security/ElasticsearchPortAvailabilityVerifier.kt
@@ -0,0 +1,80 @@
+package de.evoila.cf.elasticsearch.security
+
+/**
+ * Created by reneschollmeyer, evoila on 20.12.18.
+ */
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Socket
+
+/**
+ * @author Johannes Hiemer.
+ */
+@Service
+class ElasticsearchPortAvailabilityVerifier {
+
+    private val log = LoggerFactory.getLogger(ElasticsearchPortAvailabilityVerifier::class.java)
+
+    fun timeout(timeout: Int) {
+        try {
+            Thread.sleep(timeout.toLong())
+        } catch (e1: InterruptedException) {
+            log.info("Starting new timeout interval was interrupted.", e1)
+        }
+
+    }
+
+    private fun execute(ip: String, port: Int): Boolean {
+        var available = false
+
+        log.info("Verifying port availability on: {}:{}", ip, port)
+        val socket = Socket()
+        try {
+            socket.connect(InetSocketAddress(ip, port), SOCKET_TIMEOUT)
+
+            if (socket.isConnected)
+                available = true
+            else
+                timeout(SOCKET_TIMEOUT)
+        } catch (e: Exception) {
+            log.info("Service port could not be reached", e)
+
+            timeout(SOCKET_TIMEOUT)
+        } finally {
+            if (socket != null && socket.isConnected) {
+                try {
+                    socket.close()
+                } catch (e: IOException) {
+                    log.info("Could not close port", e)
+                }
+
+            }
+        }
+        return available
+    }
+
+    fun verifyServiceAvailability(ip: String, port: Int, useInitialTimeout: Boolean): Boolean {
+        var available = false
+
+        if (useInitialTimeout)
+            this.timeout(INITIAL_TIMEOUT)
+
+        available = this.execute(ip, port)
+
+        log.info("Service Port availability: {}", available)
+
+        log.info("Service Port availability (last status during request): {}", available)
+        return available
+    }
+
+    companion object {
+
+        private val SOCKET_TIMEOUT = 30000
+
+        private val INITIAL_TIMEOUT = 150 * 1000
+    }
+
+}
