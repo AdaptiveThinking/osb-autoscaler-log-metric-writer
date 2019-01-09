56ab46c2874642654016b4649a672842591659ea
René Schollmeyer <rschollmeyer@evoila.de>
2019-01-09 21:03:57 +0100

    refactor: remove all prometheus stuff

--- prometheus-writer/src/main/java/de/evoila/cf/prometheus/writer/constants/HttpMetricFields.kt
+++
@@ -1,9 +0,0 @@
-package de.evoila.cf.prometheus.writer.constants
-
-enum class HttpMetricFields(val httpMetric: String) {
-
-    REQUESTS("requests"),
-    LATENCY("latency"),
-    DESCRIPTION("description")
-
-}
--- prometheus-writer/src/main/java/de/evoila/cf/prometheus/writer/constants/InstanceMetricFields.kt
+++
@@ -1,9 +0,0 @@
-package de.evoila.cf.prometheus.writer.constants
-
-enum class InstanceMetricFields(val instanceMetric: String) {
-
-    CPU("cpu"),
-    RAM("ram"),
-    DESCRIPTION("description")
-
-}
