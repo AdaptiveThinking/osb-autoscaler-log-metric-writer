eac41ccacd888a6c2863bd5a3451b07de02d8ddb
René Schollmeyer <rschollmeyer@evoila.de>
2019-01-11 15:08:54 +0100

    fix: remove configuration that caused the bean to be created twice

--- core/src/main/java/de/evoila/cf/writer/configuration/WriterConfiguration.kt
+++ core/src/main/java/de/evoila/cf/writer/configuration/WriterConfiguration.kt
@@ -1,12 +1,11 @@
 package de.evoila.cf.writer.configuration
 
-import de.evoila.cf.autoscaler.kafka.KafkaPropertiesBean
 import de.evoila.cf.elasticsearch.writer.beans.ElasticsearchPropertiesBean
 import de.evoila.cf.elasticsearch.writer.beans.GrokPatternBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableConfigurationProperties(KafkaPropertiesBean::class, ElasticsearchPropertiesBean::class, GrokPatternBean::class)
+@EnableConfigurationProperties(ElasticsearchPropertiesBean::class, GrokPatternBean::class)
 open class WriterConfiguration {
 }
